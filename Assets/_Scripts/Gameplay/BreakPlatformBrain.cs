fc1e8b56e3ef225cb0cfc6f08d4721922dfb3068
Gabriel Rodriguez <gabriel.drc25.2@gmail.com>
2022-09-16 17:31:40 -0300

    BreakPlatformBrain Component
    Se creo el brain para la plataforma que quiebra. Lanzara eventos cada que se quiebra una layer.

---
+++ Assets/_Scripts/Gameplay/BreakPlatformBrain.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Game.Gameplay;
+using UnityEngine;
+
+namespace Game.Gameplay
+{
+    public class BreakPlatformBrain : MonoBehaviour
+    {
+        [SerializeField] OnPlayerOver _onPlayerOver;
+        [SerializeField, Range(0f, 5f)] float _timeToBreakLayer = 5;
+        [SerializeField] int _maxLayers = 3;
+        [SerializeField, Range(.1f, 10f)] int _timeToRespawn = 0;
+        float _time = 0;
+        int _currentLayer = 3;
+        
+        public event Action<int> OnChangeLayer;
+
+        void Awake()
+        {
+            _currentLayer = _maxLayers;
+        }
+
+        void Update()
+        {
+            if (_onPlayerOver.IsPlayerOver && _currentLayer != 0)
+            {
+                _time += 1 * Time.deltaTime;
+            }
+
+            if (_time >= _timeToBreakLayer && _currentLayer > 0)
+            {
+                _time = 0;
+                _currentLayer--;
+                OnChangeLayer?.Invoke(_currentLayer);
+            }
+
+            if (_currentLayer == 0)
+            {
+                _time += 1 * Time.deltaTime;
+                if (_time >= _timeToRespawn)
+                {
+                    _currentLayer = _maxLayers;
+                    _time = 0;
+                    OnChangeLayer?.Invoke(_currentLayer);
+                }
+            }
+        }
+    }
+}
