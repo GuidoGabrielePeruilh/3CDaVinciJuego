ec057f7f05c8cc3e965bc65754cdd0161d356238
Zebado <sbasualdo.antares@gmail.com>
2022-09-17 18:14:46 -0300

    MoveTemporal

---
+++ Assets/_Scripts/Gameplay/Move.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+    public class Move : MonoBehaviour
+    {
+        [field : SerializeField] public Vector3 Velocity { get; set; } = Vector3.zero;
+
+        [SerializeField] Rigidbody _myRg;
+
+        [SerializeField] bool _ignoreX = false;
+        [SerializeField] bool _ignoreY = false;
+        [SerializeField] bool _ignoreZ = false;
+
+        private void FixedUpdate()
+        {
+            _myRg.velocity = new Vector3(
+                 _ignoreX ? _myRg.velocity.x : Velocity.x,
+                 _ignoreY ? _myRg.velocity.y : Velocity.y,
+                 _ignoreZ ? _myRg.velocity.z : Velocity.z
+                );
+
+        }
+
+    }
+}
