a03623a79e2821b62d4eb695349c310c55005234
Gabriel Rodriguez <gabriel.drc25.2@gmail.com>
2022-09-19 21:19:27 -0300

    Ultima actualizacion

--- Assets/_Scripts/Gameplay/Platforms/BreakPlatformAC.cs
+++ Assets/_Scripts/Gameplay/Platforms/BreakPlatformAC.cs
@@ -1,9 +1,8 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Game.Gameplay
+namespace Game.Gameplay.Platforms
 {
     public class BreakPlatformAC : MonoBehaviour
     {
