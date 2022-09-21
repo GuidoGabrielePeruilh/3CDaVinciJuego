f9d095d1f6ca1b627244209260b9e3c6edc113e7
Gabriel Rodriguez <gabriel.drc25.2@gmail.com>
2022-09-21 17:27:47 -0300

    GR_Jump
    Se agrego el componente de Jump mas el inputaction para jump

---
+++ Assets/_Scripts/Gameplay/Jump.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace Game.Gameplay
+{
+    public class Jump : MonoBehaviour
+    {
+        [Header("Dependencies")]
+        [SerializeField] Rigidbody _rigidbody;
+        [SerializeField] float _jumpForce = 10f;
+
+        public void JumpAction()
+        {
+            _rigidbody.AddForce(Vector3.up * _jumpForce, ForceMode.VelocityChange);
+        }
+    }
+}
