723b21f8c609d3fa41508b94d6866bb9b4d5c9a6
Zebado <sbasualdo.antares@gmail.com>
2022-09-20 21:38:59 -0300

    SB_Platform_MOVE
    plataforma movible entre 2 puntos, si un player esta encima de ella, sera hijo de la plataforma para seguir su movimiento
    
    enemigo, un patrullaje random entre puntos, sigue al jugador en cierto rango, y realiza un ataque melee.

--- Assets/_Scripts/Gameplay/Enemies/MeleeAttack.cs
+++ Assets/_Scripts/Gameplay/Enemies/MeleeAttack.cs
@@ -6,12 +6,7 @@ namespace Game.Gameplay.Enemies
 {
     public class MeleeAttack : MonoBehaviour
     {
-        /*necesitamos:  que detecte cuando esta muy cerca y cambie el follow a attack.
-        //se necesita un ataque.
-        un objeto que ataque que aparezca en cierto momento y desaparezca
-        necesitamos que reste vida al player;              
-         */
-        [SerializeField] Transform _heatBox;        
+           
         [SerializeField] MetalEnemyAC _aniController;
         
 
