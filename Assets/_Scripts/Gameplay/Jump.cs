d15621097b9cbb3e254750c66cb58186862b0b5d
Zebado <sbasualdo.antares@gmail.com>
2022-09-22 20:25:12 -0300

    Input
    se agrego input y jump al input

--- Assets/_Scripts/Gameplay/Jump.cs
+++ Assets/_Scripts/Gameplay/Jump.cs
@@ -7,7 +7,7 @@ namespace Game.Gameplay
         [Header("Dependencies")]
         [SerializeField] Rigidbody _rigidbody;
         [SerializeField] float _jumpForce = 10f;
-
+        
         public void JumpAction()
         {
             _rigidbody.AddForce(Vector3.up * _jumpForce, ForceMode.VelocityChange);
--- Assets/_Scripts/Player/PlayerController.cs
+++ Assets/_Scripts/Player/PlayerController.cs
@@ -4,73 +4,28 @@ using UnityEngine.InputSystem;
 
 namespace Game.Player
 {
-    [RequireComponent(typeof(InputController))]
+    
     public class PlayerController : MonoBehaviour
     {
-        [SerializeField] float speed = 2f;
-        [SerializeField] float _rotationSpeed = 0f;
 
+        [SerializeField] Move _move;
+        [SerializeField] Jump _jump;
+        [SerializeField, Range(0,10)] private float _speed = 2;
 
-        InputController _inputController = null;
-
-        private void Awake()
+        public void Move(InputAction.CallbackContext context)
         {
-            _inputController = GetComponent<InputController>();
-        }
-        private void Update()
-        {
-            Move();
-        }
-        void Move()
-        {
-            Vector2 input = _inputController.MoveInput();
-
-            transform.position += transform.forward * input.y * speed * Time.deltaTime; 
+           var input = context.ReadValue<Vector2>();
+            _move.Velocity = new Vector3(input.x, 0, input.y).normalized * _speed;
         }
 
-        /*
-        [SerializeField] InputAction _moveInput = null;
-        [SerializeField] InputAction _cameraInput = null;
-        Rigidbody _myRg;
-        
-        private void Start()
+       public void Jump(InputAction.CallbackContext context)
         {
-            _myRg = GetComponent<Rigidbody>();
-        }
-        private void OnEnable()
-        {
-            _moveInput.Enable();
-            _cameraInput.Enable();
-        }
-        private void OnDisable()
-        {
-            _moveInput.Disable();
-            _cameraInput.Disable();
-        }
-        public Vector2 MoveInput()
-        {
-            return _moveInput.ReadValue<Vector2>();
-        }
-        public Vector2 CameraInput()
-        {
-            return _cameraInput.ReadValue<Vector2>();    
-        }
-        private void FixedUpdate()
-        {
-            _myRg.velocity = transform.position * speed * Time.fixedDeltaTime;
+            if (context.performed)
+            {
+                _jump.JumpAction();
+            }
         }
         
-        [SerializeField] PlayerInput playerInput;
-        Vector3 input;
-        void OnAttack(InputValue valor)
-        {
-            print("ataque");
-        }
-        void OnMovimiento(InputValue valor)
-        {
-            Vector2 inputMovement = valor.Get<Vector2>();
-            input = new Vector3(inputMovement.x, 0, inputMovement.y);
-        }
-        */
+        
     }
 }
