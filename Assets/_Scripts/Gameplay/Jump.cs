3ec454e5bff615d268b7b0492a804fd15b3aa5fa
Zebado <sbasualdo.antares@gmail.com>
2022-09-22 21:04:29 -0300

    GR_Jump
    Verificacion de la colision con el suelo

--- Assets/_Scripts/Gameplay/Jump.cs
+++ Assets/_Scripts/Gameplay/Jump.cs
@@ -7,10 +7,37 @@ namespace Game.Gameplay
         [Header("Dependencies")]
         [SerializeField] Rigidbody _rigidbody;
         [SerializeField] float _jumpForce = 10f;
-
+        [SerializeField] LayerMask _floorLayer;
+        [SerializeField] GameObject _cheeckFloor;
+        [SerializeField, Range(0, 2)] float _floorRadius;
+        bool _canJump = true;
+        void Update()
+        {
+            OnTheFloor();
+        }
         public void JumpAction()
         {
-            _rigidbody.AddForce(Vector3.up * _jumpForce, ForceMode.VelocityChange);
+            if (_canJump)
+            {
+                _canJump = false;
+                _rigidbody.AddForce(Vector3.up * _jumpForce, ForceMode.VelocityChange);
+            }            
+        }
+        public void OnTheFloor()
+        {
+            if (Physics.OverlapSphere(_cheeckFloor.transform.position, _floorRadius, _floorLayer).Length >= 1)
+            {
+                _canJump = true;
+            }
+            else if (Physics.OverlapSphere(_cheeckFloor.transform.position, _floorRadius, _floorLayer).Length <= 1)
+            {
+                _canJump = false;
+            }
+        }
+        void OnDrawGizmos()
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawSphere(_cheeckFloor.transform.position, _floorRadius);
         }
     }
 }
