25a7f3b3482ac26b6d27d8e549d907ab0fe0b956
mrubio98 <mrubio_98@hotmail.com>
2022-09-26 15:43:12 -0300

    MR_HealthUIController
    IntSO de Max Health y actualización del script que controla la barra de vida

---
+++ Assets/_Scripts/UI/HealthUIController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Game.SO;
+using Game.Player;
+
+namespace Game.UI
+{
+    public class HealthUIController : MonoBehaviour
+    {
+        [SerializeField] Slider _slider;
+        [SerializeField] Image _sliderFillImage;
+        [SerializeField] Gradient _colorVariention;
+
+        [Header("Player's Life")]
+        [SerializeField] IntSO _maxPlayerLife;
+        [SerializeField] IntSO _actualPlayerLife;
+
+        private void Start()
+        {
+            SetSliderValue();
+        }
+
+        private void Update()
+        {
+            SetSliderValue();
+        }
+
+        public void SetSliderValue()
+        {
+            float actualValue = _actualPlayerLife.value;
+            float maxValue = _maxPlayerLife.value;
+
+            float value = actualValue / maxValue;
+            _slider.value = value;
+            _sliderFillImage.color = _colorVariention.Evaluate(value);
+        }
+
+
+    }
+}
