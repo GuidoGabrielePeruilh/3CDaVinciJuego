04eb222d0771932f2a62f09fadcdf960c5b7dbe2
Zebado <sbasualdo.antares@gmail.com>
2022-10-08 19:51:50 -0300

    SB_GR_Bugs-y-Mejoras
    Se soluciono el doble salto y el coyote

--- Assets/_Scripts/Gameplay/Jump.cs
+++ Assets/_Scripts/Gameplay/Jump.cs
@@ -8,50 +8,60 @@ namespace Game.Gameplay
         [Header("Dependencies")]
         [SerializeField] Rigidbody _rigidbody;
         [SerializeField] float _jumpForce = 10f;
-        [SerializeField] int _floorLayer;
+        [SerializeField] LayerMask _floorLayer;
+        [SerializeField] GameObject _cheeckFloor;
         [SerializeField, Range(0, 2)] float _floorRadius;
+        [SerializeField] float _maxExtraTime = 1;
         bool _onTheFloor = true;
-        [SerializeField, Range(0f, 2f)] float _maxExtraTime;
-        float _extraTime = 0;
         bool _alreadyJump = false;
-
-        void Awake()
-        {
-            transform.localScale = new Vector3(_floorRadius, _floorRadius, _floorRadius);
-        }
+        float _extraTime = 0;
 
         void Update()
         {
+            OnTheFloor();
             if (!_onTheFloor && !_alreadyJump)
             {
                 _extraTime += 1 * Time.deltaTime;
             }
         }
-
-        void OnTriggerEnter(Collider other)
-        {
-            // if (other.gameObject.layer != _floorLayer) return;
-
-            _alreadyJump = false;
-            _onTheFloor = true;
-        }
-
-        void OnTriggerExit(Collider other)
-        {
-            // if (other.gameObject.layer != _floorLayer) return;
-
-            _onTheFloor = false;
-            _extraTime = 0;
-        }
-
         public void JumpAction()
         {
-            if ((_onTheFloor || _extraTime < _maxExtraTime) && !_alreadyJump)
+            if (_alreadyJump) return;
+            if (_onTheFloor || (!_onTheFloor && _extraTime < _maxExtraTime ))
             {
                 _alreadyJump = true;
-                _onTheFloor = false;
                 _rigidbody.AddForce(Vector3.up * _jumpForce, ForceMode.VelocityChange);
             }
         }
+        public void OnTheFloor()
+        {
+            if (Physics.OverlapSphere(_cheeckFloor.transform.position, _floorRadius, _floorLayer).Length >= 1)
+            {
+                if (!_onTheFloor)
+                {
+                    _alreadyJump = false;
+                    _extraTime = 0;
+                }
+                _onTheFloor = true;
+            }
+            else if (Physics.OverlapSphere(_cheeckFloor.transform.position, _floorRadius, _floorLayer).Length <= 1)
+            {
+                _onTheFloor = false;
+            }
+        }
+#if UNITY_EDITOR
+
+        void OnDrawGizmos()
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawSphere(_cheeckFloor.transform.position, _floorRadius);
+        }
+#endif
     }
 }
+
+
+
+
+
+
